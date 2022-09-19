ae296d1b598d46d252acf76cc708a78786bbf295
riyajindal <riyajindal@2808gmail.com>
2022-09-19 22:16:24 +0530

    Some more Sorting Algos added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from 'react'
-import './App.css'
-import SortingVisualizer from './SortingVisualizer/SortingVisualizer';
+import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
-      <SortingVisualizer></SortingVisualizer>
+    <div className="App">
+      <div className="container">
+          <Header />
+          <Home />
+      </div>
+    </div>
   );
 }
 
